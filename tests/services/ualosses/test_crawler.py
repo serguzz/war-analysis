d0b59b8a91c386383ac532e43f70a1ef1d38c78f
Sergiy Brovchenko <bb219@brokbridge.com>
2026-09-07 15:40:15 +0300

    First crawler tests

---
+++ tests/services/ualosses/test_crawler.py
@@ -0,0 +1,36 @@
+import pytest
+
+from src.services.ualosses_parser.crawler import next_prefix
+
+
+@pytest.mark.parametrize(
+    ("prefix", "expected"),
+    [
+        # Empty.
+        ("", ""),
+
+        # Symbols.
+        ("'", "-"),
+        ("-", "a"),
+
+        # Single letters.
+        ("a", "b"),
+        ("b", "c"),
+        ("y", "z"),
+        ("z", ""),
+
+        # Multiple letters.
+        ("aa", "ab"),
+        ("ab", "ac"),
+        ("ay", "az"),
+        ("az", "b"),
+        ("za", "zb"),
+        ("zy", "zz"),
+        ("zz", ""),
+    ],
+)
+def test_next_prefix(
+    prefix: str,
+    expected: str,
+):
+    assert next_prefix(prefix) == expected
