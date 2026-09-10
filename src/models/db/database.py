2b299385f28284249f9ad3da440b1e7a534c35d3
Sergiy Brovchenko <bb219@brokbridge.com>
2026-09-10 12:49:22 +0300

    sessionmaker to DB

--- src/models/db/database.py
+++ src/models/db/database.py
@@ -1,13 +1,18 @@
 import os
 
 from sqlalchemy import create_engine
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
 
 DATABASE_URL = os.environ["DATABASE_URL"]
 
 engine = create_engine(DATABASE_URL)
 
+SessionLocal = sessionmaker(
+    bind=engine,
+    autoflush=False,
+    expire_on_commit=False,
+)
 
 class Base(DeclarativeBase):
     pass
