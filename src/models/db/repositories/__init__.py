880935005a6b96577ad04abb1dcf555eab8ffd7f
Sergiy Brovchenko <bb219@brokbridge.com>
2026-09-10 14:14:18 +0300

    init for imports

---
+++ src/models/db/repositories/__init__.py
@@ -0,0 +1,15 @@
+from .location import LocationRepository
+from military_unit import MilitaryUnitRepository
+from place import PlaceRepository
+from soldier import SoldierRepository
+from soldier_source import SoldierSourceRepository
+from source import SourceRepository
+
+__all__ = [
+    "LocationRepository",
+    "MilitaryUnitRepository",
+    "PlaceRepository",
+    "SoldierRepository",
+    "SoldierSourceRepository",
+    "SourceRepository",
+]
